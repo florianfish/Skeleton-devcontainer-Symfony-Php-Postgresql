1306b7e70ea4d7bb41f0f515ccdf4c1735487314
Florian <florian@poissonmail.fr>
2025-04-18 08:07:44 +0000

    adding rabbitmq with messenger

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Message\SmsNotification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -11,6 +13,17 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/message-test', name: 'app_message_test')]
+    public function messageTest(MessageBusInterface $bus): Response
+    {
+        $bus->dispatch(new SmsNotification('Look! I created a message!'));
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
